de269c9ee192eed46b240d2619c16b25a4196d6a
Kingson Yang <kingsony1025@gmail.com>
2023-11-09 23:20:54 -0500

    fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -67,15 +67,17 @@ export const Contact = () => {
                     </Col>
                     <Col size={12} className="px-1">
                       <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                      <br/>
                     </Col>
-                    {
-                      status.message &&
-                      <Col>
+                  </Row>
+
+                  <Row>
+                    <Col size={12} className="px-1">
+                        <button type="submit"><span>{buttonText}</span></button>
+                        {
+                        status.message &&
                         <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                      </Col>
                     }
+                    </Col>
                   </Row>
                 </form>
               </div>}
